1f3ee7aa59bb0bf27cca7b394f455dfa3786e75c
TrvMarco <trevisanello.marco@gmail.com>
2022-06-01 19:16:38 +0200

    Created Footer bottom

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <BaseMain/>
     <FooterTop/>
     <FooterMid/>
+    <FooterBottom/>
   </div>
 </template>
 
@@ -12,6 +13,7 @@ import BaseHeader from './components/Header/BaseHeader'
 import BaseMain from './components/Main/BaseMain'
 import FooterTop from './components/Footer/FooterTop'
 import FooterMid from './components/Footer/FooterMid.vue'
+import FooterBottom from './components/Footer/FooterBottom.vue'
 
 export default {
   name: 'App',
@@ -20,12 +22,12 @@ export default {
     BaseMain,
     FooterTop,
     FooterMid,
+    FooterBottom
   }
 }
 </script>
 
 <style lang="scss">
-@import './assets/style/global.scss';
 @import './assets/style/global.scss';
 </style>
  
---
+++ src/components/Footer/FooterBottom.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="footer-bottom">
+      <div class="container">
+          <div class="footer-left">
+              <button class="btn">Sign-up now!</button>
+          </div>
+          <div class="footer-right">
+              <nav>
+                  <ul class="social-links">
+                      <li><h3>Follow us</h3></li>
+                      <li><a href="#"><img src="../../assets/img/footer-facebook.png" alt=""></a></li>
+                      <li><a href="#"><img src="../../assets/img/footer-twitter.png" alt=""></a></li>
+                      <li><a href="#"><img src="../../assets/img/footer-youtube.png" alt=""></a></li>
+                      <li><a href="#"><img src="../../assets/img/footer-pinterest.png" alt=""></a></li>
+                  </ul>
+              </nav>
+          </div>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'FooterBottom'
+}
+</script>
+
+<style lang="scss">
+@import '../../assets/style/mixins.scss';
+    .footer-bottom{
+        background-color: var(--footer-bottom-bg-color);
+        padding: 1.25rem 0;
+
+        & .container{
+            display: flex;
+            justify-content: space-between;
+            align-items: center
+        }
+    }
+
+    .social-links{
+        @include listStyle;
+        align-items: center;
+        gap: 1.25rem;
+
+        & h3{
+            text-transform: uppercase;
+            color: var(--primary-color);
+        }
+    }
+
+</style>
