527f907ff532e867291402cdd1ad372255965e89
TrvMarco <trevisanello.marco@gmail.com>
2022-06-06 16:01:16 +0200

    Added obj for footer mid links

--- src/components/Footer/FooterMid.vue
+++ src/components/Footer/FooterMid.vue
@@ -3,53 +3,34 @@
         <div class="container">
             <div class="col-sx">
                 <div class="col">
-                    <h3>Dc Comics</h3>
-                    <nav>
-                        <ul class="footers-links">
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                        </ul>
-                    </nav>
-                    <h3>Shop</h3>
-                    <nav>
-                        <ul class="footers-links">
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                        </ul>
-                    </nav>
+                    <div v-for="(link,index) in colOne" :key="index">
+                        <h3>{{link.title}}</h3>
+                        <nav>
+                            <ul class="footers-links">
+                                <li v-for="(link,index) in link.singleLink" :key="index"><a href=""><small>{{link.text}}</small></a></li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
                 <div class="col">
-                    <h3>Dc</h3>
-                    <nav>
-                        <ul class="footers-links">
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                        </ul>
-                    </nav>
+                    <div v-for="(link,index) in colTwo" :key="index">
+                        <h3>{{link.title}}</h3>
+                        <nav>
+                            <ul class="footers-links">
+                                <li v-for="(link,index) in link.singleLink" :key="index"><a href=""><small>{{link.text}}</small></a></li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
                 <div class="col">
-                    <h3>Sites</h3>
-                    <nav>
-                        <ul class="footers-links">
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                            <li><a href=""><small>link</small></a></li>
-                        </ul>
-                    </nav>
+                    <div v-for="(link,index) in colThree" :key="index">
+                        <h3>{{link.title}}</h3>
+                        <nav>
+                            <ul class="footers-links">
+                                <li v-for="(link,index) in link.singleLink" :key="index"><a href=""><small>{{link.text}}</small></a></li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
             </div>
             <div class="col-dx"></div>
@@ -59,7 +40,138 @@
 
 <script>
 export default {
-    name: 'FooterMid'
+    name: 'FooterMid',
+    data(){
+        return {
+            colOne: [
+                {
+                    title: 'Dc comics',
+                    singleLink: [
+                        {
+                            text: 'Characters',
+                            url: '#'
+                        },
+                        {
+                            text: 'Comics',
+                            url: '#'
+                        },
+                        {
+                            text: 'Movies',
+                            url: '#'
+                        },
+                        {
+                            text: 'TV',
+                            url: '#'
+                        },
+                        {
+                            text: 'Games',
+                            url: '#'
+                        },
+                        {
+                            text: 'Videos',
+                            url: '#'
+                        },
+                        {
+                            text: 'News',
+                            url: '#'
+                        },
+                    ]
+                },
+                {
+                    title: 'Shop',
+                    singleLink: [
+                        {
+                            text: 'Shop DC',
+                            url: '#'
+                        },
+                        {
+                            text: 'Shop DC Collectibles',
+                            url: '#'
+                        },
+                    ]   
+                }
+
+            ],
+            colTwo:[
+                {
+                    title: 'Dc',
+                    singleLink: [
+                        {
+                            text: 'Terms Of Use',
+                            url: '#'
+                        },
+                        {
+                            text: 'Privacy policy (New)',
+                            url: '#'
+                        },
+                        {
+                            text: 'Ad Choices',
+                            url: '#'
+                        },
+                        {
+                            text: 'Advertising',
+                            url: '#'
+                        },
+                        {
+                            text: 'Jobs',
+                            url: '#'
+                        },
+                        {
+                            text: 'Subscriptions',
+                            url: '#'
+                        },
+                        {
+                            text: 'Talent Workshops',
+                            url: '#'
+                        },
+                        {
+                            text: 'CPSC Certificates',
+                            url: '#'
+                        },
+                        {
+                            text: 'Ratings',
+                            url: '#'
+                        },
+                        {
+                            text: 'Shop Help',
+                            url: '#'
+                        },
+                        {
+                            text: 'Contact Us',
+                            url: '#'
+                        },
+                    ]
+                }
+            ],
+            colThree: [
+                {
+                    title: 'Sites',
+                    singleLink: [
+                        {
+                            text: 'DC',
+                            url: '#'
+                        },
+                        {
+                            text: 'MAD Magazine',
+                            url: '#'
+                        },
+                        {
+                            text: 'DC Kids',
+                            url: '#'
+                        },
+                        {
+                            text: 'DC Universe',
+                            url: '#'
+                        },
+                        {
+                            text: 'DC Power Visa',
+                            url: '#'
+                        }
+                    ]
+                }
+            ]    
+        }
+    }
 }
 </script>
 
